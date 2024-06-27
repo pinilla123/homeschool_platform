1c14fe65f8dc549d553773008213c4c983458681
pinilla123 <nicolas.pinilla123@gmail.com>
2024-06-27 17:05:04 -0400

    feat: Implement createClient function for Supabase SSR integration

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,13 @@
+// utils/client.ts
+
+// Import the createBrowserClient function from the @supabase/ssr package
+// This function helps in creating a Supabase client that works well with server-side rendering (SSR)
 import { createBrowserClient } from "@supabase/ssr";
 
+// Define a function to create and return a Supabase client
+// This function uses environment variables for the Supabase URL and anonymous key
 export const createClient = () =>
   createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!, // The URL of your Supabase project
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, // The anonymous key for your Supabase project
   );
